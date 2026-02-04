82a90a0860f75638551c5f7155a51234776280f0
MuratAcar00 <muratacar1152@gmail.com>
2026-02-04 21:16:03 +0300

    part1 is completed

---
+++ ft_calloc.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: muracar <muracar@student.42istanbul.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/04 17:57:27 by muracar           #+#    #+#             */
+/*   Updated: 2026/02/04 17:57:27 by muracar          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_calloc(size_t count, size_t size)
+{
+    void	*ptr;
+
+    if (count !=  0 && (count * size) / count != size)
+            return (NULL);
+    
+    ptr = malloc(count * size);
+    if (!ptr)
+        return (NULL);
+    ft_bzero(ptr, count * size);
+    return (ptr);
+}
---
+++ ft_strdup.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: muracar <muracar@student.42istanbul.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/04 21:15:14 by muracar           #+#    #+#             */
+/*   Updated: 2026/02/04 21:15:14 by muracar          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strdup(const char *s1)
+{
+    char	*dup;
+    size_t	len;
+    size_t	i;
+
+    len = ft_strlen(s1);
+    dup = (char *)malloc((len + 1) * sizeof(char));
+    if (!dup)
+        return (NULL);
+    i = 0;
+    while (i < len)
+    {
+        dup[i] = s1[i];
+        i++;
+    }
+    dup[i] = '\0';
+    return (dup);
+}
